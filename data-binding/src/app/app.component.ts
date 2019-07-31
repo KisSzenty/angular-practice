89bf9bdb7ff3d81e79b5ea2f58f7ab202918abe9
KisSzenty <kisszenty@gmail.com>
2019-07-31 15:11:53 +0200

    complete

--- data-binding/src/app/app.component.ts
+++ data-binding/src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-
+  title: string = `angular-practice-001`;
+  getName(): string {
+    return `Bond, James Bond`;
+  }
 }
