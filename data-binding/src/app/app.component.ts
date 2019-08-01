5b2a111409bf279d34e80d549c21b8ecf1cf1db9
KisSzenty <kisszenty@gmail.com>
2019-08-01 09:19:23 +0200

    practice

--- data-binding/src/app/app.component.ts
+++ data-binding/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
+import { summaryForJitFileName } from '@angular/compiler/src/aot/util';
 
-@Component({
+@Component({ //metadata
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
@@ -9,5 +10,25 @@ export class AppComponent {
   title: string = `angular-practice-001`;
   getName(): string {
     return `Bond, James Bond`;
+
+  }
+  names: string[] = [
+    'Pisti',
+    'Sanyi',
+    'Laci',
+    'Vali'
+  ];
+  name: string;
+  disabled: boolean = false;
+
+  constructor() {
+    this.name = this.names[0];
+    setInterval(() => {
+      let index: number = Math.floor(Math.random() * this.names.length)
+    }, 2000);
+  }
+  setName(name): void {
+    this.name = name;
   }
 }
+
